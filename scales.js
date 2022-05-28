fffadf59179fb2f5b58f49c774ea2de4f850861a
Chris Castellano <c.castellano18@gmail.com>
2022-05-27 22:14:16 -0500

    bandscale

--- scales.js
+++ scales.js
@@ -3,20 +3,39 @@ const svg = d3.select('svg');
 d3.json('menu.json').then((data) => {
   //join data to rects
   const rects = svg.selectAll('rect').data(data);
-  const y = d3.scaleLinear().domain([0, 1000]).range([0, 500]);
+  const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.orders)])
+    .range([0, 500]);
+
+  //min
+  // const min = d3.min(data, (d) => d.orders);
+
+  // //max
+  // const max = d3.max(data, (d) => d.orders);
+
+  // //extent (min and max)
+  // const extent = d3.extent(data, (d) => d.orders);
+  //bandscale
+  const x = d3
+    .scaleBand()
+    .domain(data.map((item) => item.name))
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
 
   rects
-    .attr('width', 50)
+    .attr('width', x.bandwidth)
     .attr('height', (d) => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70);
+    .attr('x', (d) => x(d.name));
 
   //append enter selection to DOM
   rects
     .enter()
     .append('rect')
-    .attr('width', 50)
+    .attr('width', x.bandwidth)
     .attr('height', (d) => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70);
+    .attr('x', (d) => x(d.name));
 });
