058dd8ae494c38aad06d549817d034a0415da94c
Chris Castellano <c.castellano18@gmail.com>
2022-05-27 22:35:17 -0500

    groups

--- scales.js
+++ scales.js
@@ -1,8 +1,21 @@
-const svg = d3.select('svg');
+const svg = d3
+  .select('.canvas')
+  .append('svg')
+  .attr('width', 600)
+  .attr('height', 600);
+
+//create margins and dimensions
+const margin = { top: 20, right: 20, bottom: 100, left: 100 };
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg
+  .append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 d3.json('menu.json').then((data) => {
-  //join data to rects
-  const rects = svg.selectAll('rect').data(data);
   const y = d3
     .scaleLinear()
     .domain([0, d3.max(data, (d) => d.orders)])
@@ -23,6 +36,9 @@ d3.json('menu.json').then((data) => {
     .range([0, 500])
     .paddingInner(0.2)
     .paddingOuter(0.2);
+
+  //join data to rects
+  const rects = graph.selectAll('rect').data(data);
 
   rects
     .attr('width', x.bandwidth)
