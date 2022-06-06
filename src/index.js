b27a37f17c3605c89f70232c22fcb52cbab2e42c
Chris Castellano <c.castellano18@gmail.com>
2022-06-05 20:39:21 -0500

    graph complete

--- src/index.js
+++ src/index.js
@@ -35,6 +35,8 @@ btns.forEach((btn) => {
 
     //set text of form span
     formAct.textContent = activity;
+
+    update(data);
   });
 });
 
@@ -73,8 +75,8 @@ const graph = svg
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 //scales
-const x = d3.scaleTime([0, graphWidth]);
-const y = d3.scaleLinear([graphHeight, 0]);
+const x = d3.scaleTime().range([0, graphWidth]);
+const y = d3.scaleLinear().range([graphHeight, 0]);
 
 //axes group
 const xAxisGroup = graph
@@ -84,19 +86,120 @@ const xAxisGroup = graph
 
 const yAxisGroup = graph.append('g').attr('class', 'y-axis');
 
+//line path generator
+const line = d3
+  .line()
+  .x(function (d) {
+    return x(new Date(d.date));
+  })
+  .y(function (d) {
+    return y(d.distance);
+  });
+
+const path = graph.append('path');
+
+//create dotted lines
+const dottedLine = graph.append('g').attr('class', 'line').style('opacity', 0);
+
+const xDottedLine = dottedLine
+  .append('line')
+  .attr('stroke', '#aaa')
+  .attr('stroke-width', 1)
+  .attr('stroke-dasharray', 4);
+
+const yDottedLine = dottedLine
+  .append('line')
+  .attr('stroke', '#aaa')
+  .attr('stroke-width', 1)
+  .attr('stroke-dasharray', 4);
+
 //graph update
 const update = (data) => {
+  //filter for activity
+  data = data.filter((item) => item.activity == activity);
+  console.log(data);
+  //sort data by date objects
+  data.sort((a, b) => new Date(a.date) - new Date(b.date));
+
   //domains
   x.domain(d3.extent(data, (d) => new Date(d.date)));
   y.domain([0, d3.max(data, (d) => d.distance)]);
 
+  //update path data
+  path
+    .data([data])
+    .attr('fill', 'none')
+    .attr('stroke', '#00bfa5')
+    .attr('stroke-width', 2)
+    .attr('d', line);
+  //create circles
+  const circles = graph.selectAll('circle').data(data);
+
+  //remove unwanted
+  circles.exit().remove();
+
+  //update current points
+  circles
+    .attr('cx', (d) => x(new Date(d.date)))
+    .attr('cy', (d) => y(d.distance));
+
+  //add points
+  circles
+    .enter()
+    .append('circle')
+    .attr('r', 4)
+    .attr('cx', (d) => x(new Date(d.date)))
+    .attr('cy', (d) => d.distance)
+    .attr('fill', '#ccc');
+
+  graph
+    .selectAll('circle')
+    .on('mouseover', (event, d) => {
+      d3.select(event.currentTarget)
+        .transition()
+        .duration(100)
+        .attr('r', 8)
+        .attr('fill', '#fff');
+
+      xDottedLine
+        .attr('x1', x(new Date(d.date)))
+        .attr('x2', x(new Date(d.date)))
+        .attr('y1', graphHeight)
+        .attr('y2', y(d.distance));
+
+      yDottedLine
+        .attr('x1', 0)
+        .attr('x2', x(new Date(d.date)))
+        .attr('y1', y(d.distance))
+        .attr('y2', y(d.distance));
+
+      dottedLine.style('opacity', 1);
+    })
+    .on('mouseleave', (event, d) => {
+      d3.select(event.currentTarget)
+        .transition()
+        .duration(100)
+        .attr('r', 4)
+        .attr('fill', '#ccc');
+
+      dottedLine.style('opacity', 0);
+    });
   //axes
-  const xAxis = d3.axisBottom(x).ticks(4);
-  const yAxis = d3.axisLeft(y).ticks(4);
+  const xAxis = d3.axisBottom(x).ticks(4).tickFormat(d3.timeFormat('%b %d'));
+  const yAxis = d3
+    .axisLeft(y)
+    .ticks(4)
+    .tickFormat((d) => d + ' miles');
 
   //call axes
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
+
+  //rotate text
+  xAxisGroup
+    .selectAll('text')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end');
 };
 
 //graph
